2f7fe5c1e67c50bce182b64690456ab1bd6219a8
allyssons <allyssonsrodrigues@gmail.com>
2015-07-10 10:35:25 -0300

    Matriz do ano passado, pra aprender

---
+++ Main.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include "Matriz.h"
+
+using namespace std;
+
+int main(){
+	int opt;
+	int i, j;
+	int linha, coluna;
+	cout << "Programa para realizar operação com matrizes";
+	cout << endl<<"Qual operação deseja realizar??"<<endl;
+	cout << "0 - Sair" << endl;
+	cout << "1 - Soma"<<endl;
+	cout << "2 - Subtração"<<endl;
+	cout << "3 - Multiplicação"<<endl;
+	cout << "4 - Transposta"<<endl;
+	cin >> opt;	
+
+	while(opt){
+		cout<<"Digite o número de linhas e de colunas da primeira matriz: ";
+		cin>>linha>>coluna;
+		Matriz<int> *A = new Matriz<int>(linha, coluna);
+		cout<<"Digite o número de linhas e de colunas da segunda matriz: ";
+		cin>>linha>>coluna;
+		Matriz<int> *B = new Matriz<int>(linha, coluna);
+		Matriz<int> *C = new Matriz<int>(); 
+		
+		int a;
+		cout<<"Digite os valores da primeira matriz"<<endl;
+		for (i = 0; i < A->getnl(); ++i){
+			for (j = 0; j < A->getnc(); ++j){
+				cin >> a;
+				A->set(i, j, a);
+			}
+		}
+		cout<<"Digite os valores da segunda matriz"<<endl;
+		for (i = 0; i < B->getnl(); ++i){
+			for (j = 0; j < B->getnc(); ++j){
+				cin >> a;
+				B->set(i, j, a);
+			}
+		}
+		//Soma
+		if (opt == 1){
+			if ((A->getnl() == B->getnl()) && (A->getnc() == B->getnc())){
+				*C = *A + *B;
+				cout << endl;
+				for (i = 0; i < C->getnl(); ++i){
+					for (j = 0; j < C->getnc(); ++j){
+						cout << C->get(i, j) << ' ';
+					}
+					cout << endl;
+				}
+			}
+			else
+				cout<< endl << "Matrizes não podem ser operadas" << endl;
+		}
+		
+		//Subtração
+		if (opt == 2){
+			if ((A->getnl() == B->getnl()) && (A->getnc() == B->getnc())){
+				*C = *A - *B;
+				cout << endl;
+				for (i = 0; i < C->getnl(); ++i){
+					for (j = 0; j < C->getnc(); ++j){
+						cout << C->get(i, j) << ' ';
+					}
+					cout << endl;
+				}
+			}
+			else
+				cout<< endl << "Matrizes não podem ser operadas" << endl;
+		}
+		
+		//Multiplicação
+		if (opt == 3){
+			if ((A->getnc() == B->getnl())){
+				*C = *A * *B;
+				cout << endl;
+				for (i = 0; i < C->getnl(); ++i){
+					for (j = 0; j < C->getnc(); ++j){
+						cout << C->get(i, j) << ' ';
+					}
+					cout << endl;
+				}
+			}
+			else
+				cout << endl << "Matrizes não podem ser operadas" << endl;
+		}		
+	
+		//Transposta
+		if (opt == 4){
+			cout << endl;
+			cout << "A:" << endl;
+			*C = *A / *B;
+			for (i = 0; i < C->getnl(); ++i){
+				for (j = 0; j < C->getnc(); ++j){
+					cout << C->get(i, j) << ' ';
+				}
+				cout << endl;
+			}
+			cout << endl;
+			cout << "B:" << endl;
+			*C = *B / *A;
+		
+			for (i = 0; i < C->getnl(); ++i){
+				for (j = 0; j < C->getnc(); ++j){
+					cout << C->get(i, j) << ' ';
+				}
+				cout << endl;
+			}
+		}
+		char aux;// variavel auxiliar para travar o programa
+		cout << endl << "[PRESS ANY KEY TO CONTINUE]" << endl;
+		cin.get(aux);
+		cin.get(aux);
+		//Nova chamada do menu
+		cout << endl<<"Qual operação deseja realizar??"<<endl;
+		cout << "0 - Sair" << endl;
+		cout << "1 - Soma"<<endl;
+		cout << "2 - Subtração"<<endl;
+		cout << "3 - Multiplicação"<<endl;
+		cout << "4 - Transposta"<<endl;
+		cin >> opt;
+	}
+	return 0;
+}
---
+++ Matriz.h
@@ -0,0 +1,115 @@
+#ifndef MATRIZ_H
+#define MATRIZ_H
+#include <iostream>
+
+using namespace std;
+
+//Classe da matriz.
+template <typename T> class Matriz{
+private:
+	int nl, nc;
+	T **M;
+public:
+   Matriz():nl(0), nc(0), M(NULL){};
+	
+	Matriz(int l, int c):nl(l), nc(c){
+		M = new T*[nl];
+		for (int i = 0; i < nl; ++i){
+			M[i] = new T[nc];
+			for (int j = 0; j < nc; ++j) M[i][j] = 0;
+		}			
+	}
+	
+   ~Matriz();
+	
+	int getnl();
+	int getnc();
+	void set(int i, int j, const T &val);
+	T get(int i, int j);
+	
+	Matriz<T>& operator+(Matriz<T> &B);
+	Matriz<T>& operator-(Matriz<T> &B);
+	Matriz<T>& operator*(Matriz<T> &B);
+	Matriz<T>& operator/(Matriz<T> &B);
+};
+
+//D
+template <typename T> Matriz<T>::~Matriz(){
+	for (int i = 0; i < nl; ++i) delete[]M[i];
+	delete M;
+	M = NULL;
+}
+
+template <typename T> int Matriz<T>::getnl(){
+	return nl;
+}
+
+template <typename T> int Matriz<T>::getnc(){
+	return nc;
+}
+
+template <typename T> void Matriz<T>::set(int i, int j, const T &val){
+	M[i][j] = val;
+}
+
+template <typename T> T Matriz<T>::get(int i, int j){
+	return (M[i][j]);
+}
+
+//Sobrecarga de operador mais.
+template <typename T> Matriz<T>& Matriz<T>::operator+(Matriz<T> &B){
+	Matriz<int> *temp = new Matriz<int>(getnl(), getnc());
+	
+	for (int i = 0; i < getnl(); ++i){
+		for (int j = 0; j < getnc(); ++j){
+			temp->set(i, j, get(i, j) + B.get(i, j));
+		}
+	}
+	
+	return *temp;
+}
+
+
+// Sobrecarga de operador menos.
+template <typename T> Matriz<T>& Matriz<T>::operator-(Matriz<T> &B){
+	Matriz<int> *temp = new Matriz<int>(getnl(), getnc());
+	
+	for (int i = 0; i < getnl(); ++i){
+		for (int j = 0; j < getnc(); ++j){
+			temp->set(i, j, get(i, j) - B.get(i, j));
+		}
+	}
+	
+	return *temp;
+}
+
+// Sobrecarga de operador vezes.
+template <typename T> Matriz<T>& Matriz<T>::operator*(Matriz<T> &B){
+
+	int  auxresul=0;
+	Matriz<int> *temp = new Matriz<int>(getnl(), B.getnc());
+	for (int i = 0; i < getnl(); ++i){
+		for (int j = 0; j < B.getnc(); ++j){
+			for (int v = 0; v < getnc(); ++v){
+				auxresul += (get(i, v) * B.get(v, j));
+			}
+		temp -> set(i, j, auxresul);
+		auxresul = 0;
+		}
+	}
+	return *temp;
+}
+
+// Sobrecarga de operador transposta.
+template <typename T> Matriz<T>& Matriz<T>::operator/(Matriz<T> &B){
+	Matriz<int> *temp = new Matriz<int>(getnc(), getnl());
+		for (int i = 0; i < getnl(); i++){
+			for(int j = 0; j < getnc(); j++){
+					temp->set(j, i, get(i, j));
+			}
+		}
+	return *temp;
+}
+
+#endif
+
